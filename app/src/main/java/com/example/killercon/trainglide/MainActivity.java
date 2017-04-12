dc926fa94f250e931f8c51aed13b2e93bce1d659
Panachai <panachai.ny@gmail.com>
2017-04-13 01:13:45 +0700

    complete

--- app/src/main/java/com/example/killercon/trainglide/MainActivity.java
+++ app/src/main/java/com/example/killercon/trainglide/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.killercon.trainglide;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +15,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ImageView imageView = (ImageView)findViewById(R.id.imageView1);
 
-        
+        Context context = getApplicationContext();
+
+        Glide.with(context)
+                .load("http://www.metalbridges.com/wp-content/uploads/2016/05/pokemonSM-1-e1462949488571.jpg")
+                .diskCacheStrategy(DiskCacheStrategy.ALL)   //บังคับให้ Glide เซฟรูปทุกขนาด ลง Cache
+                .into(imageView);
     }
+
+
 }
